ce8363afb1da0e078aab0f9bd95f4f5c9e0528c6
Sam Li <weisen.li@hotmail.com>
2025-04-20 15:46:49 +0000

    add email confirmation page

---
+++ src/routes/email-confirmation/+page.svelte
@@ -0,0 +1,12 @@
+<div class="min-h-screen flex items-center justify-center">
+  <div class="text-center">
+      <h1 class="text-2xl font-bold">确认邮箱已发送</h1>
+      <p class="my-4">请前往注册邮箱激活</p>
+  </div>
+</div>
+
+<style>
+  :global(body) {
+      background-color: #FEF2F2; /* Adjust based on your design */
+  }
+</style>
