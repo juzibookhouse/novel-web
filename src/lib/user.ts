114df51f5369bdcdf90bcb2d00ff7770304d547d
09liweis <weisen.li@hotmail.com>
2025-05-06 15:37:44 -0400

    fix display datetime

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -15,7 +15,15 @@ export const isValidMembership = (user:User) => {
 }
 
 export const getUserDateFormat = (date:string) => {
-  return new Date(date).toLocaleDateString('zh-CN');
+  const options = { 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  };
+  return new Date(date).toLocaleDateString('zh-CN',options);
 }
 
 export const getUserMembershipDate = (user:User) => {
