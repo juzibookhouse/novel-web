fd91373cb97c853092760589db9e87c10da33294
09liweis <weisen.li@hotmail.com>
2025-05-08 10:45:06 -0400

    refactor chapter pagination

---
+++ src/lib/components/novels/ChapterPagination.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let novelId,prevChapterId,nextChapterId;
+</script>
+<!-- Chapter Navigation Buttons -->
+<div
+class="p-6 border-t-2 border-red-100 flex justify-between items-center"
+>
+{#if prevChapterId}
+  <a
+    href={`/novel/${novelId}/chapter/${prevChapterId}`}
+    class="px-6 py-2 bg-red-100 text-primary rounded-full hover:bg-red-200 transition-colors duration-200"
+  >
+    ← 上一章
+  </a>
+{:else}
+  <div></div>
+{/if}
+
+{#if nextChapterId}
+  <a
+    href={`/novel/${novelId}/chapter/${nextChapterId}`}
+    class="px-6 py-2 bg-red-100 text-primary rounded-full hover:bg-red-200 transition-colors duration-200"
+  >
+    下一章 →
+  </a>
+{:else}
+  <div></div>
+{/if}
+</div>
--- src/routes/novel/[novelId]/chapter/[chapterId]/+page.svelte
+++ src/routes/novel/[novelId]/chapter/[chapterId]/+page.svelte
@@ -7,6 +7,7 @@
   import MembershipPlans from "$lib/components/MembershipPlans.svelte";
   import { getUserDateFormat } from "$lib/user.js";
   import { getChapterLength } from "$lib/novel.js";
+    import ChapterPagination from "$lib/components/novels/ChapterPagination.svelte";
 
   export let data;
   $: ({ chapter, prevChapterId, nextChapterId, novelId } = data);
@@ -187,32 +188,8 @@
       {/if}
     </div>
 
-    <!-- Chapter Navigation Buttons -->
-    <div
-      class="p-6 border-t-2 border-red-100 flex justify-between items-center"
-    >
-      {#if prevChapterId}
-        <a
-          href={`/novel/${novelId}/chapter/${prevChapterId}`}
-          class="px-6 py-2 bg-red-100 text-primary rounded-full hover:bg-red-200 transition-colors duration-200"
-        >
-          ← 上一章
-        </a>
-      {:else}
-        <div></div>
-      {/if}
-
-      {#if nextChapterId}
-        <a
-          href={`/novel/${novelId}/chapter/${nextChapterId}`}
-          class="px-6 py-2 bg-red-100 text-primary rounded-full hover:bg-red-200 transition-colors duration-200"
-        >
-          下一章 →
-        </a>
-      {:else}
-        <div></div>
-      {/if}
-    </div>
+    <ChapterPagination novelId={novelId} prevChapterId={prevChapterId} nextChapterId={nextChapterId} />
+    
   </div>
 </div>
 
