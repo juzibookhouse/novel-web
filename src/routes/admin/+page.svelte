4951eac014bfb1ee8f0e7457129247a761f5e42c
Siri Yeung <491763280@qq.com>
2025-07-13 15:12:54 +0000

    add cursor pointer to admin tabs

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -79,7 +79,7 @@
         <nav class="flex -mb-px" aria-label="Tabs">
           {#each [{ id: "users", name: "读者管理" }, { id: "authors", name: "作家管理" }, { id: "novels", name: "作品管理" }, { id: "categories", name: "分类管理" }, { id: "tags", name: "标签管理" }, { id: "forms", name: "表单管理" }, { id: "reading-time", name: "阅读时间" }] as tab}
             <button
-              class="w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm
+              class="w-1/4 py-4 px-1 cursor-pointer text-center border-b-2 font-medium text-sm
                 {activeTab === tab.id
                 ? 'border-red-800 underline'
                 : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'}"
