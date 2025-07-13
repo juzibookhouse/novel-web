013cfef9ef91d3142e64cec825056d8e03a4e59a
Siri Yeung <491763280@qq.com>
2025-07-13 13:09:15 +0000

    refactor and update vip text

--- src/lib/novel.ts
+++ src/lib/novel.ts
@@ -4,25 +4,26 @@ const NOVEL_STATUS:{[key:string]:string} = {
 }
 
 const PUBLIC_TEXT = '开放阅读';
+const VIP_TEXT = '仅对会员阅读';
 
 export const FREE_OPTIONS = [
   {value: "", label:"取决于章节"},
   {value: "public", label:PUBLIC_TEXT},
   {value: "private", label:"仅对注册用户阅读"},
-  {value: "vip", label:"仅对开通会员开发阅读"}
+  {value: "vip", label:VIP_TEXT}
 ]
 
 export const CHAPTER_FREE_OPTIONS = [
   {value: "", label:"取决于小说"},
   {value: "public", label:PUBLIC_TEXT},
   {value: "private", label:"仅对注册用户阅读"},
-  {value: "vip", label:"仅对开通会员开发阅读"}
+  {value: "vip", label:VIP_TEXT}
 ]
 
 export const FREE_OPTIONS_MAP:{[key:string]:string} = {
   public: PUBLIC_TEXT,
   private: '仅对注册用户阅读',
-  vip: '仅对开通会员开发阅读',
+  vip: VIP_TEXT,
 }
 
 export interface Category {
