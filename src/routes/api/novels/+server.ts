3e22e7625053ce6eb6872a798beda640e1b2b50e
Siri Yeung <491763280@qq.com>
2025-09-02 15:23:17 +0000

    call getAuthUser in novels api

--- src/routes/api/novels/+server.ts
+++ src/routes/api/novels/+server.ts
@@ -1,8 +1,13 @@
+import { getAuthUser } from '$lib/server/auth.js';
 import { supabase } from '$lib/supabaseClient';
 import { json } from '@sveltejs/kit';
 
-export async function GET({ }) {
+export async function GET({ request }) {
   try {
+    const {user_id} = await getAuthUser(request);
+    if (!user_id) {
+      return json({ error: 'User not authenticated' }, { status: 401 });
+    }
     const { data: novels, error } = await supabase
       .from('novels')
       .select(`
