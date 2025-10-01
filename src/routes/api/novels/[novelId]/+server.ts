0bcb558c4130ec964475cfccf6d8c2cd8be11568
Siri Yeung <491763280@qq.com>
2025-10-01 13:46:49 +0000

    add delete novel api

---
+++ src/routes/api/novels/[novelId]/+server.ts
@@ -0,0 +1,36 @@
+import { json } from '@sveltejs/kit';
+import { supabase } from '$lib/supabaseClient';
+import { getAuthUser } from '$lib/server/auth.js';
+
+export async function DELETE({ params, request }) {
+  try {
+    const { novelId } = params;
+    const {user_id} =  await getAuthUser(request);
+    if (!user_id) {
+      return json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const {error: ReadingError } = await supabase.from('reading_records').delete().eq('novel_id', novelId);
+
+    if (ReadingError) {
+      return json({ error: 'Failed to delete reading records' }, { status: 500 });
+    }
+
+    const { error: chaptersError } = await supabase.from('chapters').delete().eq('novel_id', novelId);
+
+    if (chaptersError) {
+      return json({ error: 'Failed to delete chapters' }, { status: 500 });
+    }
+
+  // Then delete the novel itself
+    const {error: novelError} = await supabase.from('novels').delete().eq('id', novelId);
+    if (novelError) {
+      return json({ error: 'Failed to delete novel' }, { status: 500 });
+    }
+
+    return json({ msg: 'Comment deleted successfully' });
+  } catch (err) {
+    console.error('Comments API error:', err);
+    return json({ error: 'Internal server error' }, { status: 500 });
+  }
+}
