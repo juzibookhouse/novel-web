554899704d177e0da3ba312ef5c8484aca1eb9bb
Sam Li <weisen.li@hotmail.com>
2025-10-04 11:21:12 +0000

    add cusor pointer to delete chapter

--- src/lib/components/author/NovelChapterForm.svelte
+++ src/lib/components/author/NovelChapterForm.svelte
@@ -103,7 +103,7 @@
             <button 
               type="button" 
               on:click={deleteChapterHandler}
-              class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+              class="inline-flex cursor-pointer justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
             >
               删除章节
             </button>
