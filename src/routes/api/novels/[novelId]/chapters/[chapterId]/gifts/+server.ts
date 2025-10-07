f9c9db833d4f1ce0b4a47fe5edc6a391c0d725df
Sam Li <weisen.li@hotmail.com>
2025-10-07 14:01:02 +0000

    add chapter gifts api

---
+++ src/routes/api/novels/[novelId]/chapters/[chapterId]/gifts/+server.ts
@@ -0,0 +1,56 @@
+import { json } from '@sveltejs/kit';
+import { supabase } from '$lib/supabaseClient';
+import { getAuthUser } from '$lib/server/auth.js';
+import type { Comment } from '$lib/types/comment.js';
+
+export async function GET({ params }) {
+  try {
+    const { chapterId } = params;
+
+    return json({ chapterId });
+  } catch (err) {
+    console.error('Comments API error:', err);
+    return json({ error: 'Internal server error' }, { status: 500 });
+  }
+}
+
+export async function POST({ request, params, locals }) {
+  try {
+    const { chapterId, novelId } = params;
+    const {user_id} = await getAuthUser(request);
+    const {gift_id} = await request.json();
+    
+    if (!user_id) {
+      return json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    if (!gift_id) {
+      return json({ error: 'Gift id is required' }, { status: 400 });
+    }
+
+    const { data: gift, error: giftError } = await supabase.from('gifts').select('*').eq('id', gift_id).single();
+    
+    if (giftError || !gift) {
+      return json({ error: `Gift ${gift_id} not found` }, { status: 404 });
+    }
+
+    const { data: novel, error: novelError } = await supabase.from('novels').select('*').eq('id', novelId).single();
+    
+    if (novelError || !novel) {
+      return json({ error: `Novel ${novelId} not found` }, { status: 404 });
+    }
+
+    const { data: chapter, error: chapterError } = await supabase.from('chapters').select('*').eq('id', chapterId).single();
+    
+    if (chapterError || !chapter) {
+      return json({ error: `Chapter ${chapterId} not found` }, { status: 404 });
+    }
+
+    
+
+    return json({ msg: 'Send Gift successfully'}, { status: 201 });
+  } catch (err) {
+    console.error('Comments API error:', err);
+    return json({ error: 'Internal server error' }, { status: 500 });
+  }
+}
