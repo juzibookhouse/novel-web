b1c260335bc3fbd2537cfc6ae7047fd2b6d219da
Siri Yeung <491763280@qq.com>
2025-10-15 14:14:14 +0000

    create or update chapter gifts

--- src/routes/api/novels/[novelId]/chapters/[chapterId]/gifts/+server.ts
+++ src/routes/api/novels/[novelId]/chapters/[chapterId]/gifts/+server.ts
@@ -1,7 +1,6 @@
 import { json } from '@sveltejs/kit';
 import { supabase } from '$lib/supabaseClient';
 import { getAuthUser } from '$lib/server/auth.js';
-import type { Comment } from '$lib/types/comment.js';
 
 export async function GET({ params }) {
   try {
@@ -18,7 +17,7 @@ export async function POST({ request, params, locals }) {
   try {
     const { chapterId, novelId } = params;
     const {user_id} = await getAuthUser(request);
-    const {gift_id} = await request.json();
+    const {gift_id, stripe_client_secret} = await request.json();
     
     if (!user_id) {
       return json({ error: 'Unauthorized' }, { status: 401 });
@@ -46,7 +45,34 @@ export async function POST({ request, params, locals }) {
       return json({ error: `Chapter ${chapterId} not found` }, { status: 404 });
     }
 
-    
+    const {data: chapterGift, error: chapterGiftError} = await supabase.from('chapter_gifts').select('*').eq('chapter_id', chapterId).eq('gift_id', gift_id).eq('stripe_client_secret', stripe_client_secret).eq('user_id',user_id).single();
+    if (!chapterGift) {
+      const {error} = await supabase
+      .from('chapter_gifts')
+      .insert({
+        novel_id: novelId,
+        chapter_id: chapterId,
+        gift_id: gift_id,
+        user_id: user_id,
+        stripe_client_secret: stripe_client_secret,
+        payment_status: 'pending'
+      });
+      if (error) {
+        console.error('Error creating chapter gift:', error);
+        return json({ error: 'Internal server error' }, { status: 500 });
+      }
+    } else {
+      const {error} = await supabase
+      .from('chapter_gifts')
+      .update({
+        gift_id
+      })
+      .eq('id', chapterGift.id)
+      if (error) {
+        console.error('Error updating chapter gift:', error);
+        return json({ error: 'Internal server error' }, { status: 500 });
+      }
+    }
 
     return json({ msg: 'Send Gift successfully'}, { status: 201 });
   } catch (err) {
