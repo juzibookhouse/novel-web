d5f624f195035ca6788d7e7ac70b98ca638f5180
Sam Li <weisen.li@hotmail.com>
2025-10-16 02:33:26 +0000

    refactor ReceivedGifts

--- src/lib/components/novels/ChapterGifts.svelte
+++ src/lib/components/novels/ChapterGifts.svelte
@@ -13,6 +13,7 @@
     mountPaymentElement,
     type PaymentMethod
   } from "$lib/utils/stripePayment";
+    import ReceivedGifts from "./ReceivedGifts.svelte";
 
   export let chapterId: string;
   export let novelId: string;
@@ -154,16 +155,7 @@
 
 {#if $user}
 <div class="bg-gray-50 p-6 rounded-xl shadow-sm mx-auto max-w-md">
-  {#if chapterGifts.length > 0}
-    <div class="mb-4">
-      <h3 class="text-lg font-medium mb-2 text-gray-700">已收到的打赏</h3>
-      <div class="flex space-x-1 overflow-x-auto pb-1">
-        {#each chapterGifts as gift}
-          <div class="text-3xl mb-1">{gift.image}</div>
-        {/each}
-      </div>
-    </div>
-  {/if}
+  <ReceivedGifts {chapterGifts} />
   {#if !showPaymentForm}
     <GiftSelectors
       {gifts}
---
+++ src/lib/components/novels/ReceivedGifts.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let chapterGifts: any[];
+</script>
+
+{#if chapterGifts.length > 0}
+  <div class="mb-4">
+    <h3 class="text-lg font-medium mb-2 text-gray-700">已收到的打赏</h3>
+    <div class="flex space-x-1 overflow-x-auto pb-1">
+      {#each chapterGifts as gift}
+        <div class="text-3xl mb-1">{gift.image}</div>
+      {/each}
+    </div>
+  </div>
+{/if}
