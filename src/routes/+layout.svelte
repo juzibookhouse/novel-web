2a2c742af38ab55099b0d778cf513bdf1dbcd3d4
SiriYeung <119325339+SiriYeung@users.noreply.github.com>
2025-10-27 14:42:06 +0000

    add GA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,14 @@
 <svelte:head>
   <link href="https://fonts.googleapis.com/css2?family=Ma+Shan+Zheng&display=swap" rel="stylesheet">
   <meta name="viewport" content="width=1024">
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SJITD11ECX"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);} 
+    window.gtag = window.gtag || gtag;
+    gtag('js', new Date());
+    gtag('config', 'G-SJITD11ECX', { 'send_page_view': true });
+  </script>
 </svelte:head>
 
 <Header />
