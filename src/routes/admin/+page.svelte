06db5800346ade9d7ca95b68b01d497c25b12edc
Sam Li <weisen.li@hotmail.com>
2025-11-02 19:34:06 +0000

    remove admin tab underline

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -81,7 +81,7 @@
             <button
               class="w-1/4 py-4 px-1 cursor-pointer text-center border-b-2 font-medium text-sm
                 {activeTab === tab.id
-                ? 'border-red-800 underline'
+                ? 'border-red-800'
                 : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'}"
               on:click={() => (activeTab = tab.id as any)}
             >
