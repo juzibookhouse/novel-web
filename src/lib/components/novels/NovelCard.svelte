d7470a139fa8e770ab248e84d06d4e767903aadb
Sam Li <weisen.li@hotmail.com>
2025-12-14 15:50:06 +0000

    refactor novel card html

--- src/lib/components/novels/NovelCard.svelte
+++ src/lib/components/novels/NovelCard.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-    import { COVER_PLACEHOLDER } from "$lib/constants";
+  import { COVER_PLACEHOLDER } from "$lib/constants";
   import { getNovelStatus } from "$lib/novel";
-    import NovelCatTags from "./NovelCatTags.svelte";
+  import NovelCatTags from "./NovelCatTags.svelte";
 
   export let novel;
 </script>
@@ -17,7 +17,11 @@
       class="w-full object-cover transition duration-300"
     />
     <div class="absolute top-3 right-3">
-      <span class="{novel.status === 'ongoing' ? 'bg-orange-500' : 'bg-green-500'} text-white px-2 py-1 rounded text-sm">
+      <span
+        class="{novel.status === 'ongoing'
+          ? 'bg-orange-500'
+          : 'bg-green-500'} text-white px-2 py-1 rounded text-sm"
+      >
         {getNovelStatus(novel)}
       </span>
     </div>
@@ -26,19 +30,16 @@
     <h3 class="text-2xl font-medium text-gray-900">{novel.title}</h3>
     <p class="text-gray-600 line-clamp-2">{novel.description}</p>
     <div class="flex flex-wrap gap-2">
-      {#if novel.is_short }
-      <NovelCatTags catTags={[{name:'短篇'}]} type='cat' />
+      {#if novel.is_short}
+        <NovelCatTags catTags={[{ name: "短篇" }]} type="cat" />
       {/if}
-      <NovelCatTags catTags={[novel.categories]} type='cat' />
+      <NovelCatTags catTags={[novel.categories]} type="cat" />
       <NovelCatTags catTags={novel.tags} />
     </div>
-    <div class="flex items-center justify-between">
-      <!-- <span class="text-sm text-gray-500">{novel.author || "佚名"}</span> -->
-      <span
-        class="text-red-600 group-hover:translate-x-2 transition-transform duration-300"
-      >
-        阅读更多 →
-      </span>
-    </div>
+    <span
+      class="text-red-600 group-hover:translate-x-2 transition-transform duration-300"
+    >
+      阅读更多 →
+    </span>
   </div>
 </a>
